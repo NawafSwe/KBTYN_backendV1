b3d4a5f3fe6434760943b370755f0a2c946f9bc7
Nawaf <Nawaf.jr9@hotmial.com>
2020-07-19 21:57:09 +0300

    removing unceesary files

--- src/controllers/adminController.js
+++
@@ -1,7 +0,0 @@
-/* ----------------------------- importing packages ----------------------------- */
-const Customer = require('../models/customer');
-const Trip = require('../models/trip');
-const User = require('../models/user');
-
-
-
